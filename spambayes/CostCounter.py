2a03276bea08b705040076b82d7974b3ae27dc41
hooft <hooft@00d3a0d1-4e35-0410-80d6-d2ffc5a8d771>
2002-11-19 17:41:28 +0000

    More different cost counters as optimization targets

--- spambayes/CostCounter.py
+++ spambayes/CostCounter.py
@@ -13,7 +13,7 @@ class CostCounter:
         pass
 
     def __str__(self):
-        return "%s: $%.2f" % (self.name, self.total)
+        return "%s: $%.4f" % (self.name, self.total)
 
 class CompositeCostCounter:
     def __init__(self,cclist):
@@ -31,6 +31,28 @@ class CompositeCostCounter:
         s = []
         for c in self.clients:
             s.append(str(c))
+        return '\n'.join(s)
+
+class DelayedCostCounter(CompositeCostCounter):
+    def __init__(self,cclist):
+        CompositeCostCounter.__init__(self,cclist)
+        self.spamscr=[]
+        self.hamscr=[]
+
+    def spam(self, scr):
+        self.spamscr.append(scr)
+
+    def ham(self, scr):
+        self.hamscr.append(scr)
+
+    def __str__(self):
+        for scr in self.spamscr:
+            CompositeCostCounter.spam(self,scr)
+        for scr in self.hamscr:
+            CompositeCostCounter.ham(self,scr)
+        s=[]
+        for line in CompositeCostCounter.__str__(self).split('\n'):
+            s.append('Delayed-'+line)
         return '\n'.join(s)
 
 class StdCostCounter(CostCounter):
@@ -64,9 +86,32 @@ class FlexCostCounter(CostCounter):
     def ham(self, scr):
         self.total += self._lambda(scr) * options.best_cutoff_fp_weight
 
+class Flex2CostCounter(FlexCostCounter):
+    name = "Flex**2 Cost"
+    def spam(self, scr):
+        self.total += (1 - self._lambda(scr))**2 * options.best_cutoff_fn_weight
+
+    def ham(self, scr):
+        self.total += self._lambda(scr)**2 * options.best_cutoff_fp_weight
+
 def default():
      return CompositeCostCounter([
-                                  StdCostCounter(),
-                                  FlexCostCounter(),
-                                 ])
+                StdCostCounter(),
+                FlexCostCounter(),
+                Flex2CostCounter(),
+                DelayedCostCounter([
+                    StdCostCounter(),
+                    FlexCostCounter(),
+                    Flex2CostCounter(),
+                ])
+            ])
 
+if __name__=="__main__":
+    cc=default()
+    cc.ham(0)
+    cc.spam(1)
+    cc.ham(0.5)
+    cc.spam(0.5)
+    options.spam_cutoff=0.7
+    options.ham_cutoff=0.4
+    print cc
