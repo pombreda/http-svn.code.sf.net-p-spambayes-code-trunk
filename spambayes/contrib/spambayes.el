6eb22418eb1bf5b86f80c4e888b2dbe711f2c26a
npickett <npickett@00d3a0d1-4e35-0410-80d6-d2ffc5a8d771>
2003-01-27 21:12:47 +0000

    * Change comment on spambayes.el * added muttrc

--- spambayes/contrib/spambayes.el
+++ spambayes/contrib/spambayes.el
@@ -1,6 +1,6 @@
 ;; spambayes.el -- integrate spambayes into Gnus
 ;; Copyright (C) 2003 Neale Pickett <neale@woozle.org>
-;; Time-stamp: <2003-01-21 20:54:15 neale>
+;; Time-stamp: <2003-01-27 13:12:14 neale>
 
 ;; This is free software; you can redistribute it and/or modify it under
 ;; the terms of the GNU General Public License as published by the Free
@@ -19,6 +19,15 @@
 ;; Purpose:
 ;;
 ;; Functions to put spambayes into Gnus.  
+;;
+;; This assumes you are already filtering with a procmail recipie
+;; similar to:
+;;
+;;   :0fw
+;;   | hammiefilter.py -t
+;;
+;; If you can't run procmail on all your incoming messages, you shold
+;; have a look at spam.el, which is included with Oort Gnus.
 ;;
 ;; This binds "B s" to "refile as spam", and "B h" to "refile as ham".
 ;; After refiling, the message is rescored and respooled.  I haven't yet
@@ -45,37 +54,49 @@
 (defvar spambayes-hammiefilter "~/src/spambayes/hammiefilter.py"
   "Path to the hammiefilter program")
 
-(defun spambayes-retrain (args)
+(defun spambayes-retrain (is-spam)
   "Retrain on all processable articles, or the one under the cursor.
 
-This will replace the buffer contents with command output."
-  (labels ((do-exec (n g args)
+This will replace the buffer contents with command output.  You can then
+respool the article.
+
+is-spam is a boolean--true if you want to retrain the message as spam,
+false if you want to retrain as ham.
+"
+  (labels ((do-exec (n group is-spam)
+		    (message "Retraining...")
 		    (with-temp-buffer
-		      (gnus-request-article-this-buffer n g)
-		      (shell-command-on-region (point-min) (point-max)
-					       (concat spambayes-hammiefilter " " args)
-					       (current-buffer)
-					       t)
-		      (gnus-request-replace-article n g (current-buffer)))))
-    (let ((g gnus-newsgroup-name)
+		      (gnus-request-article-this-buffer n group)
+		      (shell-command-on-region
+		       (point-min)
+		       (point-max)
+		       (concat
+			spambayes-hammiefilter
+			(if is-spam " -s" " -g")
+			" -f")
+		       (current-buffer)
+		       t)
+		      (gnus-request-replace-article n group (current-buffer)))
+		    (message "Retrained article.")))
+    (let ((group gnus-newsgroup-name)
 	  (list gnus-newsgroup-processable))
       (if (>= (length list) 1)
 	  (while list
 	    (let ((n (car list)))
-	      (do-exec n g args))
+	      (do-exec n group is-spam))
 	    (setq list (cdr list)))
 	(let ((n (gnus-summary-article-number)))
-	  (do-exec n g args))))))
+	  (do-exec n group is-spam))))))
 
 (defun spambayes-refile-as-spam ()
   "Retrain and refilter all process-marked messages as spam, then respool them"
   (interactive)
-  (spambayes-retrain "-s -f")
+  (spambayes-retrain 't)
   (gnus-summary-respool-article nil (gnus-group-method gnus-newsgroup-name)))
 
 (defun spambayes-refile-as-ham ()
   "Retrain and refilter all process-marked messages as ham, then respool them"
   (interactive)
-  (spambayes-retrain "-g -f")
+  (spambayes-retrain nil)
   (gnus-summary-respool-article nil (gnus-group-method gnus-newsgroup-name)))
 
